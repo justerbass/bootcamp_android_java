f5459162a8b63780c6d85c3a842a591a63ef8a10
Javier Castillo <jkttzars@gmail.com>
2024-08-21 19:50:52 -0400

    aun no abre la app por error del fragment

--- Indicidual11/app/src/main/java/cl/bootcamp/indicidual11/MainActivity.java
+++ Indicidual11/app/src/main/java/cl/bootcamp/indicidual11/MainActivity.java
@@ -1,6 +1,7 @@
 package cl.bootcamp.indicidual11;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -8,15 +9,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button exit_btn;
+    private Button fragment_btn;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        exit_btn = findViewById(R.id.button_exit);
+        fragment_btn = findViewById(R.id.button_show_fragment);
+
+        fragment = new Fragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainerView, fragment)
+                .commit();
+
+        exit_btn.setOnClickListener(view -> finishAffinity());
+
+        fragment_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, fragment)
+                        .commit();
+            }
+
+
+        });
 
 
 
