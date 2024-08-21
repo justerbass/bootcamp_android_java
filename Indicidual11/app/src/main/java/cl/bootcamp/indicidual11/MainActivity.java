1eaf9a71ee79f2c0e421fa70058da28c549b21e5
Javier Castillo <jkttzars@gmail.com>
2024-08-21 15:37:04 -0400

    commit previo reinicio PC

--- Indicidual11/app/src/main/java/cl/bootcamp/indicidual11/MainActivity.java
+++ Indicidual11/app/src/main/java/cl/bootcamp/indicidual11/MainActivity.java
@@ -1,6 +1,7 @@
 package cl.bootcamp.indicidual11;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
 
 
 
