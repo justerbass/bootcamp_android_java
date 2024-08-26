7e6d967f214f4c5df6b1e2f3c806912d67ab77ef
Javier Castillo <jkttzars@gmail.com>
2024-08-25 22:38:31 -0400

    logica primera pantalla lista

--- SpintFinal/app/src/main/java/cl/bootcamp/spintfinal/MainActivity.java
+++ SpintFinal/app/src/main/java/cl/bootcamp/spintfinal/MainActivity.java
@@ -1,6 +1,8 @@
 package cl.bootcamp.spintfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import cl.bootcamp.spintfinal.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding  binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContatMe.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
